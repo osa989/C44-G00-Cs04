afb0759252c9b76ab28b64522e434b71070cc3c4
osa989 <osamagamal9988@gmail.com>
2025-07-13 08:54:09 +0300

    video 02 finished with regions

--- CSAssignments/Program.cs
+++ CSAssignments/Program.cs
@@ -7,218 +7,209 @@ namespace CSAssignments
         static void Main(string[] args)
         {
 
-            #region Year Quarter - If Else
+        #region Year Quarter - If Else
 
-            // Console.Write("Enter Month Number Existed in First Quarter : ");
-            // int Month;
-            // bool Flag = int.TryParse(Console.ReadLine(), out Month);
+        // Console.Write("Enter Month Number Existed in First Quarter : ");
+        // int Month;
+        // bool Flag = int.TryParse(Console.ReadLine(), out Month);
 
-            // if (Month == 1)
-            // {
-            //     Console.WriteLine("Hello Jan");
-            // }
-            // else if (Month == 2)
-            // {
-            //     Console.WriteLine("Hello Feb");
-            // }
-            // else if (Month == 3)
-            // {
-            //     Console.WriteLine("Hello Mar");
-            // }
-            // else if (Month == 4)
-            // {
-            //     Console.WriteLine("Hello Apr");
-            // }
-            // else
-            // {
-            //     Console.WriteLine("Month Not Existed in First Quarter of Year");
-            // }
+        // if (Month == 1)
+        // {
+        //     Console.WriteLine("Hello Jan");
+        // }
+        // else if (Month == 2)
+        // {
+        //     Console.WriteLine("Hello Feb");
+        // }
+        // else if (Month == 3)
+        // {
+        //     Console.WriteLine("Hello Mar");
+        // }
+        // else if (Month == 4)
+        // {
+        //     Console.WriteLine("Hello Apr");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Month Not Existed in First Quarter of Year");
+        // }
 
-            #endregion
+        #endregion
 
-            #region Year Quarter - Switch Case
+        #region Year Quarter - Switch Case
 
-            // Console.Write("Enter Month Number Existed in First Quarter : ");
-            // int month;
-            // bool Flag = int.TryParse(Console.ReadLine(), out month);
+        // Console.Write("Enter Month Number Existed in First Quarter : ");
+        // int month;
+        // bool Flag = int.TryParse(Console.ReadLine(), out month);
 
-            // switch (month)
-            // {
-            //     case 1:
-            //         Console.WriteLine("Jan");
-            //         break;
-            //     case 2:
-            //         Console.WriteLine("Feb");
-            //         break;
-            //     case 3:
-            //         Console.WriteLine("Mar");
-            //         break;
-            //     case 4:
-            //         Console.WriteLine("Apr");
-            //         break;
-            //     default:
-            //         Console.WriteLine("Not Existed !");
-            //         break;
-            // }
+        // switch (month)
+        // {
+        //     case 1:
+        //         Console.WriteLine("Jan");
+        //         break;
+        //     case 2:
+        //         Console.WriteLine("Feb");
+        //         break;
+        //     case 3:
+        //         Console.WriteLine("Mar");
+        //         break;
+        //     case 4:
+        //         Console.WriteLine("Apr");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Not Existed !");
+        //         break;
+        // }
 
-            #endregion
+        #endregion
 
-            #region Student Age - If Else
+        #region Student Age - If Else
 
-            /*
-             * Age > 22 => Student Age Greater Than 22
-             * Age < 22 => Student Age Less Than 22
-             * Age = 22 => Student Age is 22
-             */
+        /*
+         * Age > 22 => Student Age Greater Than 22
+         * Age < 22 => Student Age Less Than 22
+         * Age = 22 => Student Age is 22
+         */
 
-            // Console.Write("Enter Student Age : ");
-            // int.TryParse(Console.ReadLine(), out int age);
+        // Console.Write("Enter Student Age : ");
+        // int.TryParse(Console.ReadLine(), out int age);
 
-            // if (age > 22)
-            //     Console.WriteLine("Student Age is Greater Than 22");
-            // else if (age < 22)
-            //     Console.WriteLine("Student Age Less Than 22");
-            // else
-            //     Console.WriteLine("Student Age is 22");
+        // if (age > 22)
+        //     Console.WriteLine("Student Age is Greater Than 22");
+        // else if (age < 22)
+        //     Console.WriteLine("Student Age Less Than 22");
+        // else
+        //     Console.WriteLine("Student Age is 22");
 
-            #endregion
+        #endregion
 
-            #region Student Age - Switch Case
+        #region Student Age - Switch Case
 
-            // Console.Write("Enter Student Age : ");
-            // int.TryParse(Console.ReadLine(), out int age);
+        // Console.Write("Enter Student Age : ");
+        // int.TryParse(Console.ReadLine(), out int age);
 
-            // switch (age)
-            // {
-            //     case > 22:
-            //         Console.WriteLine("Student Age Greater Than 22");
-            //         break;
-            //     case < 22:
-            //         Console.WriteLine("Student Age Less Than 22");
-            //         break;
-            //     default:
-            //         Console.WriteLine("Student Age is 22");
-            //         break;
-            // }
+        // switch (age)
+        // {
+        //     case > 22:
+        //         Console.WriteLine("Student Age Greater Than 22");
+        //         break;
+        //     case < 22:
+        //         Console.WriteLine("Student Age Less Than 22");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Student Age is 22");
+        //         break;
+        // }
 
-            #endregion
+        #endregion
 
-            #region Student Name - If Else
+        #region Student Name - If Else
 
-            /*
-             * Omar  => Hello Omar
-             * May   => Hello May
-             * Aliaa => Hello Aliaa
-             */
+        /*
+         * Omar  => Hello Omar
+         * May   => Hello May
+         * Aliaa => Hello Aliaa
+         */
 
-            // Console.Write("Enter Name : ");
-            // string name = Console.ReadLine() ?? "No Name";
+        // Console.Write("Enter Name : ");
+        // string name = Console.ReadLine() ?? "No Name";
 
-            // if (name == "Omar")
-            //     Console.WriteLine("Hello Omar");
-            // else if (name == "May")
-            //     Console.WriteLine("Hello May");
-            // else if (name == "aliaa")
-            //     Console.WriteLine("Hello aliaa");
-            // else
-            //     Console.WriteLine("NOT FOUND !");
+        // if (name == "Omar")
+        //     Console.WriteLine("Hello Omar");
+        // else if (name == "May")
+        //     Console.WriteLine("Hello May");
+        // else if (name == "aliaa")
+        //     Console.WriteLine("Hello aliaa");
+        // else
+        //     Console.WriteLine("NOT FOUND !");
 
-            #endregion
+        #endregion
 
-            #region Student Name - Switch Case
+        #region Student Name - Switch Case
 
-            //Console.Write("Enter Name : ");
-            //string name = Console.ReadLine() ?? "No Name";
+        //Console.Write("Enter Name : ");
+        //string name = Console.ReadLine() ?? "No Name";
 
-            //switch (name)
-            //{
-            //    case "Omar":
-            //        Console.WriteLine("Hello Omar");
-            //        break;
-            //    case "May":
-            //        Console.WriteLine("Hello May");
-            //        break;
-            //    case "Aliaa":
-            //        Console.WriteLine("Hello Aliaa");
-            //        break;
-            //    default:
-            //        break;
-            //}
+        //switch (name)
+        //{
+        //    case "Omar":
+        //        Console.WriteLine("Hello Omar");
+        //        break;
+        //    case "May":
+        //        Console.WriteLine("Hello May");
+        //        break;
+        //    case "Aliaa":
+        //        Console.WriteLine("Hello Aliaa");
+        //        break;
+        //    default:
+        //        break;
+        //}
 
-            #endregion
+        #endregion
 
-            // ===================== Budget - Goto in Switch =====================
+        #region Budget - Goto in Switch (Simple Version)
 
-            /*
-             * Budget is 1000 => Option 01
-             * Budget is 2000 => Option 02 + Option 01
-             * Budget is 3000 => Option 03 + Option 02 + Option 01
-             */
+        /*
+         * Budget is 1000 => Option 01
+         * Budget is 2000 => Option 02 + Option 01
+         * Budget is 3000 => Option 03 + Option 02 + Option 01
+         */
 
-            //Console.Write("Enter Your Budget : ");
-            //int.TryParse(Console.ReadLine(), out int budget);
+        // Console.Write("Enter Your Budget : ");
+        // int.TryParse(Console.ReadLine(), out int budget);
 
-            //switch (budget)
-            //{
-            //    case 3000:
-            //        Console.WriteLine("Option 01");
-            //        Console.WriteLine("Option 02");
-            //        Console.WriteLine("Option 03");
-            //        break;
-            //    case 2000:
-            //        Console.WriteLine("Option 01");
-            //        Console.WriteLine("Option 02");
-            //        break;
-            //    case 1000:
-            //        Console.WriteLine("Option 01");
-            //        break;
+        // switch (budget)
+        // {
+        //     case 3000:
+        //         Console.WriteLine("Option 01");
+        //         Console.WriteLine("Option 02");
+        //         Console.WriteLine("Option 03");
+        //         break;
+        //     case 2000:
+        //         Console.WriteLine("Option 01");
+        //         Console.WriteLine("Option 02");
+        //         break;
+        //     case 1000:
+        //         Console.WriteLine("Option 01");
+        //         break;
+        // }
 
-            //}
+        #endregion
 
+        #region Budget - Goto in Switch (Improved Version)
 
+        // Console.Write("Enter Your Budget : ");
+        // int.TryParse(Console.ReadLine(), out int budget);
 
-            // Better way
+        // switch (budget)
+        // {
+        //     case 3000:
+        //         Console.WriteLine("Option 03");
+        //         goto case 2000;
 
-            
-            Console.Write("Enter Your Budget : ");
-            int.TryParse(Console.ReadLine(), out int budget);
+        //     case 2000:
+        //         Console.WriteLine("Option 02");
+        //         goto case 1000;
 
-            switch (budget)
-            {
-                case 3000:
-                    goto case 2000;
-                    Console.WriteLine("Option 03");
-                    
-                case 2000:
-                    Console.WriteLine("Option 02");
-                    goto case 1000;
-                case 1000:
-                    Console.WriteLine("Option 01");
-                    break;
+        //     case 1000:
+        //         Console.WriteLine("Option 01");
+        //         break;
+        // }
 
-            }
+        #endregion
 
-
-
-
-
-
-
-        // ===================== Goto Example =====================
-
+        #region Goto Example
 
         Retry:
             Console.Write("Enter Your Name : ");
             string name = Console.ReadLine();
 
             if (name == "Mostafa")
-            
                 goto Retry;
-            
             else
-            
                 Console.WriteLine($"Hello {name}");
-            
+
+            #endregion           
         }
     }
 }
